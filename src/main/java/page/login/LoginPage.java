816d4c0310e24eeaeec258382de8ff5cecb73bab
Shivam Rai <shivam.rai@krishtechnolabs.local>
2024-04-04 17:02:41 +0530

    "Adding wishlist class, and enum"
    Signed-off-by: Shivam Rai <shivam.rai@krishtechnolabs.local>

--- src/main/java/page/login/LoginPage.java
+++ src/main/java/page/login/LoginPage.java
@@ -1,50 +1,41 @@
 package page.login;
+
 import common.UtilitiesCommon;
 import enums.login.LoginPageEnum;
 
-
 /**
- * @author spandit
- * @lastmodifiedby spandit 
- * This class will contain all the Login page methods
+ * @author RShivam
+ * @lastmodifiedby RShivam This class will contain all the Login page methods
  */
 public class LoginPage {
 	/**
 	 * This method is used to Login into the application
 	 * @param usernameKey
 	 * @param passwordKey
-	 * @author spandit
-	 * @lastmodifiedby spandit
+	 * @author RShivam
+	 * @lastmodifiedby RShivam
 	 */
-	
-    //updated by ShivamR
-	
-	
-	
 	public static void login(String usernameKey, String passwordKey) {
 		UtilitiesCommon.launchApplication();
 		UtilitiesCommon.click(LoginPageEnum.HOMEPAGE_ACCEPT_XPATH);
-		UtilitiesCommon.click(LoginPageEnum.LOGINPAGE_LOGIN_REGISTER_XPATH);;
+		UtilitiesCommon.click(LoginPageEnum.LOGINPAGE_LOGIN_REGISTER_XPATH);
 		String username = UtilitiesCommon.getEnvironmentData(usernameKey);
 		String password = UtilitiesCommon.getEnvironmentData(passwordKey);
 		UtilitiesCommon.log("Logging in with User name : " + username);
 		UtilitiesCommon.enterValue(LoginPageEnum.LOGINPAGE_USERNAME_TEXT_BOX_CSS, username);
-		UtilitiesCommon.enterValue(LoginPageEnum.LOGINPAGE_PASSWORD_TEXT_BOX_CSS, UtilitiesCommon.getDecryptedPassword(password));
+		UtilitiesCommon.enterValue(LoginPageEnum.LOGINPAGE_PASSWORD_TEXT_BOX_CSS,
+		UtilitiesCommon.getDecryptedPassword(password));
 		UtilitiesCommon.click(LoginPageEnum.LOGINPAGE_LOGIN_BUTTON_XPATH);
 //		UtilitiesCommon.click(LoginPageEnum.LOGIN_ACCOUNT_CLICK_XPATH);
 //		UtilitiesCommon.click(LoginPageEnum.LOGIN_SIGNOUT_CLICK_XPATH);
-		
 	}
-
-	
 	/**
 	 * This method is used to re-Login into the application after Logout
 	 * @param usernameKey
 	 * @param passwordKey
-	 * @author spandit
-	 * @lastmodifiedby spandit
+	 * @author RShivam
+	 * @lastmodifiedby RShivam
 	 */
-	//Updated by ShivamR
 	public static void reloginAfterLogout(String usernameKey, String passwordKey) {
 		UtilitiesCommon.openUrl();
 		UtilitiesCommon.click(LoginPageEnum.HOMEPAGE_ACCEPT_XPATH);
@@ -56,15 +47,5 @@ public class LoginPage {
 		UtilitiesCommon.enterValue(LoginPageEnum.LOGINPAGE_PASSWORD_TEXT_BOX_CSS,
 		UtilitiesCommon.getDecryptedPassword(password));
 		UtilitiesCommon.click(LoginPageEnum.LOGINPAGE_LOGIN_BUTTON_XPATH);
-		
-	}
-
-
-
-//	public static void logout() {
-//		UtilitiesCommon.applicationLogout();
-//		
-//		UtilitiesCommon.click(LoginPageEnum.LOGIN_SIGNOUT_CLICK_XPATH);
-//		
-//	}
+    }
 }
---
+++ src/test/java/sanity/hamleys/SanityHamleysAddProductToWishlist.java
@@ -0,0 +1,28 @@
+package sanity.hamleys;
+
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+
+import common.UtilitiesCommon;
+import io.qameta.allure.Description;
+import page.Hamleys.WishlistPage;
+/**
+ * @author RShivam
+ * @lastmodifiedby RShivam
+ * This class will contain all Sanity tests related to Add to Product as Wishlist across the Hamleys application.
+ */
+@Listeners({ listeners.ScriptExecutionListener.class })
+public class SanityHamleysAddProductToWishlist {
+	/**
+	 * @author RShivam
+	 * @lastmodifiedby RShivam
+	 */
+	@Test(testName = "Hamleys verify Wishlist Product from with login")
+	@Description("This Test is used to Verify the wishlist product with Valid Credentials")
+	public void testAddProductToWishlist() throws InterruptedException {
+		UtilitiesCommon.launchApplication();
+		WishlistPage.testProductFromHomeScreen();
+		WishlistPage.testProductDetailsPage();
+		WishlistPage.testLoginForWishlist("AdminUserName", "AdminPassword");
+	}
+}
